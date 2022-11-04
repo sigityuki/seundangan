2292539b0daba0844f086c881e1c47613165197d
sigityuki <sigityoeki@gmail.com>
2022-11-04 16:48:10 +0700

    a

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {createRoot} from "react-dom/client"
-import App from "App.jsx"
-import "style.css"
+import App from "./App.jsx"
+import "./style.css"
 
 const root = document.querySelector("#root")
 
